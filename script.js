9920cf83796c6a58cd414fd09db19f8ef17fcc3b
mariokleen <mario@kleeen.software>
2020-07-06 19:12:20 -0600

    Heores add and delete

--- script.js
+++ script.js
@@ -4,10 +4,28 @@ const card = new Vue({
   el: "#card",
   data: {
     title: "Heroes",
-    items: [
-      'Nightcrawler',
-      'Raphael',
-      'RoboCop'
-    ]
+    heroes: [
+      { title: 'Nightcrawler', desc: 'Furry, blue-skinned mutant' },
+      { title: 'Doctor Strange', desc: 'Sorcerer Supreme' },
+      { title: 'Raphael', desc: 'Only one cool but rude half-shell' },
+      { title: 'RoboCop', desc: 'Detroit’s robotic protector' },
+    ],
+    newHero: '',
+    newHeroDesc: ''
+  },
+  methods: {
+    addHero: function() {
+      if (this.newHero !== "") {
+        this.heroes.unshift({
+          title: this.newHero,
+          desc: this.newHeroDesc
+        });
+        this.newHero = "";
+        this.newHeroDesc = "";
+      }
+    },
+    deleteHero: function(index) {
+      this.heroes.splice(index, 1);
+    }
   }
 });
